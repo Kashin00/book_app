1190bd69ad722a4fa52b4d6008fbc708ff2e2a68
Matvey <matvey.kashin@nitrix.io>
2023-10-19 13:33:13 +0300

    add color and font

---
+++ book_app/book_app/Resources/ColorStorage.swift
@@ -0,0 +1,12 @@
+//
+//  ColorStorage.swift
+//  book_app
+//
+//  Created by Matvey on 19.10.2023.
+//
+
+import UIKit
+
+enum ColorStorage {
+  static let mainPink = UIColor(red: 0.867, green: 0.282, blue: 0.631, alpha: 1)
+}
