ad4a6816edfc4b465355218326f961679cf2b915
Matvey <matvey.kashin@nitrix.io>
2023-10-19 10:02:46 +0300

    remove main story board and create initial splash screen view controller

---
+++ book_app/book_app/SplashScreenViewController.swift
@@ -0,0 +1,21 @@
+//
+//  SplashScreenViewController.swift
+//  book_app
+//
+//  Created by Matvey on 19.10.2023.
+//
+
+import Foundation
+import UIKit
+
+class SplashScreenViewController: UIViewController {
+  
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    view.backgroundColor = .red
+    print("slpash")
+  }
+  
+  
+}
