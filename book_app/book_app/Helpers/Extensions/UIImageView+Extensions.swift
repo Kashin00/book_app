70b7e51c1793a4d4373374b7732363cfe66a9225
Matthew <kashin.developer@gmail.com>
2023-10-21 23:11:28 +0300

    add missing extension for image view

--- book_app/book_app/Helpers/Extensions/UIImageView+Extensions.swift
+++ book_app/book_app/Helpers/Extensions/UIImageView+Extensions.swift
@@ -5,4 +5,18 @@
 //  Created by Матвей Кашин on 21.10.2023.
 //
 
-import Foundation
+import UIKit
+
+extension UIImageView {
+  func setImageAnimatableIfNeede(with image: UIImage?) {
+    if self.image == nil {
+      UIView.transition(with: self,
+                        duration: 0.75,
+                        options: .transitionCrossDissolve,
+                        animations: { self.image = image },
+                        completion: nil)
+    } else {
+      self.image = image
+    }
+  }
+}
